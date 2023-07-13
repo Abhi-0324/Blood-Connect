789a52d9468605be5145154ad726b4cdef378e27
Abhi-0324 <chauhanabhishek2403@gmail.com>
2023-07-14 02:48:10 +0530

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const path = require("path");
-import { fileURLToPath } from 'url';
+const { fileURLToPath } from 'url';
 //dot config
 dotenv.config();
 
